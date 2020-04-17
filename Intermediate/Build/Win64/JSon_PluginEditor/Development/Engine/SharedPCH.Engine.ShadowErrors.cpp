6f89710c4c99ff44f9656b3ecb66347a891ece23
David Hurtubise <david.hurtubise@epicgames.com>
2020-04-17 11:10:12 -0400

    Add Unreal Project Files via upload
    C++ Json Module and Unreal Engine 4.24 Json Demo Map:
    Read Json Data and Write Json Data

---
+++ Intermediate/Build/Win64/JSon_PluginEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/EPICGAMES/JSon_Plugin/Intermediate/Build/Win64/JSon_PluginEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/JSon_Plugin/Private/JsonBlueprintFunctionLibrary.cpp
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JsonBlueprintFunctionLibrary.h"
+#include "JsonUtilities/Public/JsonUtilities.h"
+#include "Serialization/JsonReader.h"
+#include "misc/DateTime.h"
+#include "..\Public\JsonBlueprintFunctionLibrary.h"
+
+
+static const FString RootName("inventoryObject");
+static const FString WritePath(FPaths::ProjectSavedDir() / "Json");
+static const FString InventoryFileName("InventoryData.json");
+static const FString FilepathFull(WritePath / InventoryFileName);
+
+bool UJsonBlueprintFunctionLibrary::WriteInventoryData(FString CharName, TArray<AActor*> InventoryItems, TArray<int> InventoryCount)
+{
+	JsonObjectPtr JsonRootObject = MakeShareable(new FJsonObject);
+
+	const FString SaveDate = FDateTime::UtcNow().ToString();
+	JsonRootObject->SetStringField("SaveDate", SaveDate);
+
+	JsonRootObject->SetStringField("Character", CharName);
+
+	TArray<TSharedPtr<FJsonValue>> InventoryItemObjects;
+	for (AActor* obj : InventoryItems)
+		{
+			InventoryItemObjects.Add(MakeShareable(new FJsonValueString(obj->GetFullName())));
+		}
+
+	JsonRootObject->SetArrayField("Items", InventoryItemObjects);
+
+	TArray<TSharedPtr<FJsonValue>> InventoryItemCountArray;
+	for (int i : InventoryCount)
+		{
+			InventoryItemCountArray.Add(MakeShareable(new FJsonValueNumber(i)));
+		}
+
+	JsonRootObject->SetArrayField("ItemCount", InventoryItemCountArray);
+
+	FString OutputString;
+	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+	FJsonSerializer::Serialize(JsonRootObject.ToSharedRef(), Writer);
+
+	return FFileHelper::SaveStringToFile(OutputString, *FilepathFull);
+}
+
+bool UJsonBlueprintFunctionLibrary::ReadInventoryData(FDateTime & SaveDate, FString & CharName, TArray<FString>& InventoryItems, TArray<int>& InventoryCount)
+{
+	FString RawData;
+
+	bool bloadedfile = FFileHelper::LoadFileToString(RawData, *FilepathFull);
+
+	if (bloadedfile)
+	{
+		JsonObjectPtr JsonRootObject = MakeShareable(new FJsonObject());
+
+		TSharedRef<TJsonReader<>> JsonReader = TJsonReaderFactory<>::Create(RawData);
+
+		if (FJsonSerializer::Deserialize(JsonReader, JsonRootObject))
+		{
+			FDateTime::Parse(JsonRootObject->GetStringField("SaveDate"), SaveDate);
+
+			CharName = JsonRootObject->GetStringField("Character");
+
+			for (TSharedPtr<FJsonValue> V : JsonRootObject->GetArrayField("Items"))
+			{
+				InventoryItems.Add(V->AsString());
+			}
+
+			for (TSharedPtr<FJsonValue> V : JsonRootObject->GetArrayField("ItemCount"))
+			{
+				InventoryCount.Add(V->AsNumber());
+			}
+		}
+	}
+
+	return false;
+}
